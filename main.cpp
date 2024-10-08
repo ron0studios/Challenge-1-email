b5b7256b0a132da140cbabbd111d0476d8464948
Ron0Studios <47331292+ron0studios@users.noreply.github.com>
2024-10-08 11:00:07 +0100

    added asynchronous openmp calls

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <cpr/cpr.h>
 #include <cassert>
 #include <regex>
+#include <thread>
+#include <omp.h>
 
 
 std::string trim(std::string str) {
@@ -42,7 +44,7 @@ std::vector<std::string> getAnagramsFromHTML(std::string text){
     //std::cout << getLocationAtRegex(anagramRawData, std::regex("<\\/script><b>[a-zA-Z \\n]*<\\/b>")) << " " << anagramRawData.size() << std::endl;
     int anagramStatusLoc = getLocationAtRegex(anagramRawData, std::regex("<\\/script><b>[\\s\\S]*<\\/b>"));
     if(anagramStatusLoc==anagramRawData.size()){
-        std::cout << "no anagrams listed for name X" << std::endl;
+        std::cout << "no anagrams listed for name, " << text << std::endl;
         return {};
     }
     std::string anagramStatus = getContentInTag(anagramRawData, "b", anagramStatusLoc);
@@ -69,6 +71,68 @@ std::vector<std::string> getAnagramsFromHTML(std::string text){
 
 
 int main() {
+
+    std::vector<cpr::AsyncResponse> responses;
+    std::vector<std::string> names;
+
+    for(int i = 0; i < 437; i++){
+        responses.push_back(cpr::GetAsync(cpr::Url{"https://www.southampton.ac.uk/people"}, cpr::Parameters{{"page",std::to_string(i)}}));
+    }
+
+    #pragma omp parallel for
+    for(int i = 0; i < 437; i++){
+        cpr::Response r = responses[i].get();
+        std::string res = r.text;
+        res.erase(std::remove_if(res.begin(), res.end(), isspace), res.end());
+        res = std::regex_replace(res, std::regex("\\n"), "");
+
+        //std::smatch m;
+        //std::regex_search(res, m, std::regex(">[a-zA-Z.]+<\\/a><\\/h4>"));
+        //std::cout << m.str(0) << std::endl;
+
+        std::vector<std::string> matches;
+        std::smatch match;
+        std::regex regx(">[a-zA-Z.]+<\\/a><\\/h4>");
+        int j = 1;
+        while (std::regex_search(res, match, regx)) {
+            matches.push_back(match.str(0));
+            //std::cout << "\nMatched string is " << match.str(0) << std::endl << "and it is found at position " << match.position(0)<< std::endl;
+            j++;
+
+            // suffix to find the rest of the string.
+            res = match.suffix().str();
+        }
+
+        for(auto &j : matches){
+            std::string name = "";
+            bool toggle = false;
+            for(int k = 2; k < j.size(); k++){
+                if(j[k]=='<') break;
+                if(isupper(j[k])) toggle = true;
+                if(toggle && isalpha(j[k])) name += j[k];
+            }
+
+
+            names.push_back(name);
+            cpr::Response anagram = cpr::Get(cpr::Url{"https://new.wordsmith.org/anagram/anagram.cgi"}
+            , cpr::Parameters{{"anagram", name},{"t","500"},{"a","n"}});
+
+            std::vector<std::string> nameAnagrams = getAnagramsFromHTML(anagram.text);
+
+            std::cout << "gotten anagrams for: " << name << std::endl;
+
+        }
+
+
+
+        //int start = getLocationAtRegex(res, std::regex(">[a-zA-Z.]+<\\/a><\\/h4>"));
+        //std::cout << res.substr(start,100) << std::endl;
+
+        //std::cout << r.text.substr(0,100) << std::endl;
+    }
+
+
+
     cpr::Response r = cpr::Get(cpr::Url{"https://new.wordsmith.org/anagram/anagram.cgi?anagram=...&t=500&a=n"});
                                //cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
                                //cpr::Parameters{{"anon", "true"}, {"key", "value"}});
